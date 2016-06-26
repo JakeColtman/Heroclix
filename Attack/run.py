25e99bfaddeb0ccc73031994a9659ed3ff24b193
jakecoltman <jakecoltman@sky.com>
2016-06-26 12:16:38 +0100

    Added really basic end point
    Implements the basic rules but doesn't include anything about special
    powers

--- Attack/run.py
+++ Attack/run.py
@@ -1,16 +1,21 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, request, jsonify
 import random
 app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "Hello, World!"
 
 def roll(num_dice):
     return sum([random.randint(1, 6) for x in range(num_dice)])
 
+@app.route('/attack', methods = ["POST"])
+def attack():
+    details = request.get_json(silent = True)
+    attack, defence, damage = details["attack"], details["defence"], details["damage"]
+    totalAttack = attack + roll(2)
+    if totalAttack >= defence:
+        inflicted_damage = damage
+    else:
+        inflicted_damage = 0
 
+    return jsonify({"final_damage":inflicted_damage, "die_value": totalAttack - attack})
 if __name__ == '__main__':
-    print(roll(1))
-    #app.run(debug=True)
+    app.run(debug=True)
